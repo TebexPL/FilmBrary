18b56c852b980441e1ba15630080c12a12793355
TebexPL <tomebe1212@gmail.com>
2022-03-03 11:16:01 +0100

    Initialised empty react-native app

---
+++ Source/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+
+const App = () => {
+
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import App from './Source/App';
+import {name as appName} from './Source/app.json';
+
+AppRegistry.registerComponent(appName, () => App);
